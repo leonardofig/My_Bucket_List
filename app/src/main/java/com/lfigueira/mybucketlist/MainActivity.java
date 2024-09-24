838d732558672f8feedec4d69e265e6b3669cab4
figue <lfigueira20@gmail.com>
2024-09-24 12:12:20 +0100

    v2.1

--- app/src/main/java/com/lfigueira/mybucketlist/MainActivity.java
+++ app/src/main/java/com/lfigueira/mybucketlist/MainActivity.java
@@ -2,8 +2,6 @@ package com.lfigueira.mybucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
